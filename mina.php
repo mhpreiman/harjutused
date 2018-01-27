6c4313e1ccee1d5828b93c62461b8ffb9bfd38d1
mhpreiman <mhpreiman@gmail.com>
2018-01-28 01:45:57 +0200

    Väljasta eesnimi

---
+++ mina.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Loo oma eesnimelise väärtusega muutuja
+ * Prindi muutuja echo ka print() funktsioonidega kujul 'Minu nimi on <nimi>'
+ * Kasuta HTML märgendeid (tage)
+*/
+
+$nimi= 'Helena';
+
+echo '<p>Minu nimi on '.$nimi.'</p><br>';
+
+print '<p>Minu nimi on '.$nimi.'</p><br>';
+
+print ('<p>Minu nimi on '.$nimi.'</p><br>');
+?>
