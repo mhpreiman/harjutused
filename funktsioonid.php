93c3197d13e7c9ac737d5909cc54f7e9b4a2b45c
mhpreiman <mhpreiman@gmail.com>
2018-01-28 05:15:12 +0200

    Värvi tabeli väljad

--- funktsioonid.php
+++ funktsioonid.php
@@ -6,6 +6,13 @@ tabeli ridade ja veergude arv.
 Prindi vastava suurusega tabel, mille väljad on täidetud juhuarvudega vahemikus 10-99
 */
 
+/*
+Koosta funktsioon genereeriVarv, mis genereerib juhusliku varvi kujul #XXXXXX ning
+saadab selle funktsioonile valjastaTabel
+*/
+
+
+//Tabel
 function valjastaTabel($read, $veerud){
     echo '<table border="1">';
 
@@ -13,7 +20,7 @@ function valjastaTabel($read, $veerud){
         echo '<tr>';
 
         for ($v = 1; $v <= $veerud; $v++){
-            echo '<td>';
+            echo '<td style="background-color: '.genereeriVarv().';">';
             echo rand(10, 99);
             echo '</td>';
         }
@@ -23,3 +30,17 @@ function valjastaTabel($read, $veerud){
 }
 
 valjastaTabel(5, 7);
+
+
+
+//Värvid
+function genereeriVarv(){
+    $varv = '#';
+
+    for ($kord = 1; $kord <= 6; $kord++) {
+        $juhuTaisarv = rand(0, 15);
+        $juhuHex = dechex($juhuTaisarv);
+        $varv .= $juhuHex;
+    }
+    return $varv;
+}
