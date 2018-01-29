bc065705d0f29dc0890ead5e4f5c9f2daedaa320
mhpreiman <mhpreiman@gmail.com>
2018-01-29 08:25:55 +0200

    Väljasta juhuarvudest koosnev massiiv. Väljasta massiiviga loodud üherealine tabel

--- massiivid.php
+++ massiivid.php
@@ -19,5 +19,45 @@ echo 'See massiiv koosneb '.count($arvud).' elemendist: &emsp;';
 for($i = 0; $i < count($arvud); $i++){
     echo $arvud[$i].'&emsp;';
 }
-echo '<br>';
+echo '<hr>';
 
+
+
+/*
+Funktsioon looMassiiv, parameetriks elementide arv.
+Loob ja tagastab juhuslikest täisarvudest (0-99) koosneva massiivi.
+*/
+
+function looMassiiv($elementideArv){
+    $massiiv = array();
+
+    for($i = 0; $i < $elementideArv; $i++){
+        $massiiv[] = rand(0,99);
+    }
+    return $massiiv;
+}
+$arvudeMassiiv = looMassiiv(5);
+
+echo '<pre>';
+echo print_r($arvudeMassiiv);
+echo '</pre><br>';
+
+
+
+/*
+Funktsioon valjastaMassiiv, parameetriks massiiv,
+mille elemendid printida üherealise tabeli kujul.
+Kasutada foreach tsüklit.
+*/
+
+function valjastaMassiiv($massiiv){
+
+    echo '<table border=1><tr>';
+
+    foreach ($massiiv as $element){
+        echo '<td>'.$element.'</td>';
+    }
+
+    echo '</tr></table>';
+}
+valjastaMassiiv($arvudeMassiiv);
