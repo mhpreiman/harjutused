7eeb1f604ac5e000dcee5459e23d711a8e846897
mhpreiman <mhpreiman@gmail.com>
2018-01-30 01:05:04 +0200

    Tabeli tegemine raamatu masiivist

--- massiivid2.php
+++ massiivid2.php
@@ -53,3 +53,83 @@ foreach ($opilased as $opilane){
     echo '-------<br>';
 }
 
+
+echo '<hr>';
+
+
+/*
+Funktsioon otsiRaamat, parameetriks raamatute massiiv ja nende väljalaenutamise olek.
+Tagastab massiivi.
+Funktsioon raamatuteTabel, parameetriks massiiv.
+Tulemused tabeli kujul, mille pealkirjadeks massiivi võtmed
+Katsetamiseks:
+array(
+    array(
+        'title' => 'raamat',
+        'author' => 'autor',
+        'print' => 'trükikoda',
+        'status' => 'seisund'
+    )
+)*/
+
+
+$raamatud = array(
+    array('title' => 'Söekaevurid',
+        'author' => 'E. Zola',
+        'print' => 'Eesti Riiklik Kirjastus',
+        'status' => 'sees'
+    ),
+    array('title' => 'Läänerindel muutuseta',
+        'author' => 'E. Remarque',
+        'print' => 'Tänapäev',
+        'status' => 'valjas'
+    ),
+    array('title' => 'Faust',
+        'author' => 'J. W. Goethe',
+        'print' => 'Tartu Ülikooli Kirjastus',
+        'status' => 'sees'
+    ),
+    array('title' => 'Isa Goriot',
+        'author' => 'H. Balzac',
+        'print' => 'Kirjastus Avita',
+        'status' => 'valjas'
+    ),
+    array('title' => 'Kuritöö ja karistus',
+        'author' => 'F. Dostojevski',
+        'print' => 'Eesti Riiklik Kirjastus',
+        'status' => 'valjas')
+);
+
+
+//Raamatu tabel
+function raamatuTabel($raamatud) {
+
+    $html = '<table border="1"><thead>';
+    $getAttribute = array_keys($raamatud[0]);           //get array keys (eg 'title')
+
+
+//FIRST ROW
+    foreach ($getAttribute as $attribute) {
+        $html .= '<th>'.$attribute.'</th>';
+    }
+    $html .= '</thead>';
+
+
+//ROWS
+    foreach ($raamatud as $raamat) {
+        $html .= '<tr>';
+
+
+//COLUMNS
+        foreach ($getAttribute as $attribute) {
+            $html .= '<td>'.$raamat[$attribute].'</td>';
+        }
+        $html .= '</tr>';
+    }
+
+    $html .= '</table>';
+    echo $html;
+}
+raamatuTabel($raamatud);
+
+
