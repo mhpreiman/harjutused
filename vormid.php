cdaf33babacb344d82cab152a99f78a4821612d7
mhpreiman <mhpreiman@gmail.com>
2018-01-31 21:47:08 +0200

    Muuda numbrimängus õige vastus juhuslikuks

--- vormid.php
+++ vormid.php
@@ -21,8 +21,8 @@ function vormiAndmed(){
                 exit;
             }
         }
-        echo 'Tere, '.$kasutaja.'<br>';
-        echo 'Sinu parooliks on '.$parool.'<br>';
+//        echo 'Tere, '.$kasutaja.'<br>';
+//        echo 'Sinu parooliks on '.$parool.'<br>';
     }
 }
 //valjastaVorm();
@@ -37,22 +37,29 @@ siis teavitatakse kasutajat, et ta on õigele vastusele lähedal.
 Logi ja näita kasutajale mitu katset on juba tehtud
 */
 
-session_start();
 
 function manguVorm(){
     echo '
         <form action="'.$_SERVER['PHP_SELF'].'" method="post">
             Arva ära arv 1-15:<br>
-            <input type="text" name="kasutajaVastus"><br><br><br>
-            <input type="submit" value="Kontrolli">
+            <input type="text" name="kasutajaVastus" autocomplete="off"><br><br>
+            <input type="submit" value="Kontrolli"><br><br>
+            <input type="submit" value="Alusta uuesti" name="alustaUut">
         </form>
     ';
 }
 
 function kontrolliArv(){
-    $oigeVastus = 12;
 
-    if(!empty($_POST)){
+    session_start();
+
+    //Sets oigeVastus
+    if (!isset($_SESSION['oigeVastus']) || isset($_POST["alustaUut"])) {
+        $_SESSION['oigeVastus'] = rand(1,10);
+    }
+    $oigeVastus = $_SESSION['oigeVastus'];
+
+    if(!empty($_POST) && !isset($_POST["alustaUut"])){
         $kasutajaArv = $_POST['kasutajaVastus'];
 
         if(empty($_POST['kasutajaVastus'])){
@@ -70,8 +77,10 @@ function kontrolliArv(){
         }
         if(abs($kasutajaArv - $oigeVastus) <= 5){
             if($kasutajaArv == $oigeVastus){
-                echo 'Õige vastus on '.$oigeVastus.'! Arvasid selle ära '.$_SESSION['katseteArv'].' korraga!<br>';
+                echo 'Õige vastus on '.$oigeVastus.'!<br> Arvasid selle ära '.$_SESSION['katseteArv'].' korraga!<br>';
                 unset($_SESSION['katseteArv']);
+                unset($_SESSION['oigeVastus']);
+                session_destroy() ;
                 exit;
             }
             echo 'Sinu arv on <i>peaaegu</i> õige...<br>';
