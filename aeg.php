676543e05301f613a692b85830feccde86f2ea59
mhpreiman <mhpreiman@gmail.com>
2018-02-02 01:50:05 +0200

    Sünnikuupäeva vorm

--- aeg.php
+++ aeg.php
@@ -1,5 +1,6 @@
 <?php
 
+/*
 date_default_timezone_set('Europe/Tallinn');
 
 $aegHetkel = time();
@@ -9,5 +10,34 @@ $kuupaev = date('j. F Y');
 
 echo $kellaaeg.'<br>';
 echo $kuupaev.'<br>';
+*/
 
+
+
+/*
+Funktsioon vorm, väljastab vormi, milles eesnimi, perenimi, sünnikuupäev
+Päeva, kuu ja aasta jaoks eraldi input
+
+Funktsioon ajaTootlus, parameetrid sünnipäev, kuu, aasta,
+mis muuta andmebaasi jaoks sobilikku DATE formaati
+Prindi saadud väärtus
+*/
+
+function vorm() {
+    echo '
+        <form action="'.$_SERVER['PHP_SELF'].'" method="post">
+        Eesnimi: <input type="text" name="eesnimi"><br>
+        Perenimi: <input type="text" name="perenimi"><br><br>
+        Sünnikuupäev: <br>
+        Päev: <input type="text" name="paev"><br>
+        Kuu: <input type="text" name="kuu"><br>
+        Aasta: <input type="text" name="aasta"><br><br>
+        Sünnikuupäev ühest lahtrist: <br>
+        <input type="date" name="sunnikuupaev" value="2000-01-01"><br><br>
+        <input type="submit" value="Saada">
+        </form>
+        ';
+}
+
+vorm();
 
