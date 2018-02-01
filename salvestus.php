292f6d21f3d678d6c5f2f30e4009870c95fd86da
mhpreiman <mhpreiman@gmail.com>
2018-02-01 03:28:14 +0200

    Loodud andmete salvestamine faili

--- salvestus.php
+++ salvestus.php
@@ -14,4 +14,23 @@ function andmeteKontroll() {
     }
     return $kontroll;
 }
+
+function salvestaFaili($failinimi) {
+    if (andmeteKontroll()) {
+        if (file_exists($failinimi) and is_file($failinimi) and is_writable($failinimi)) {
+            echo 'Faili saab salvestada<br>';
+            $fail = fopen($failinimi, 'a');
+            foreach ($_POST as $vaartus){
+                fwrite($fail, $vaartus." ");
+            }
+            fwrite($fail, "\n");
+            echo 'Andmed on salvestatud<br>';
+            echo '<a href="valjund.php">Vaata faili sisu</a>';
+            fclose($fail);
+        } else {
+            echo 'Probleem '.$failinimi.' failiga<br>';
+        }
+    }
+}
 andmeteKontroll();
+salvestaFaili('andmed.txt');
