99dd26344d81a88a77e6797a4f512401d81d4f16
mhpreiman <mhpreiman@gmail.com>
2018-02-02 07:59:13 +0200

    Prindi värviline tabel

--- tabel-test.php
+++ tabel-test.php
@@ -15,3 +15,17 @@ $minuTabel->lisaRidaPealkiri(array ('Pealkiri 1'=> 'sisu9',     //pealkirjad ja 
 
 $minuTabel->prindiTabel();
 
+
+//Objekti loomine
+require_once 'tabelHTML.php';
+$htmlTabel = new tabelHTML(array('Pealkiri 1', 'Pealkiri 2', 'Pealkiri 3', 'Pealkiri 4'),'pink');
+
+//Meetodite kutsumine
+$htmlTabel->lisaRida(array('sisu1', 'sisu2', 'sisu3', 'sisu4'));
+$htmlTabel->lisaRidaPealkiri(array ('Pealkiri 1'=> 'sisu5',     //pealkirjad ja sisu korraga
+                                    'Pealkiri 3'=> 'sisu6',
+                                    'Pealkiri 2'=> 'sisu7',
+                                    'Pealkiri 4'=> 'sisu18'));
+$htmlTabel->lisaRida(array('sisu9', 'sisu10', 'sisu11', 'sisu12'));
+
+$htmlTabel->prindiTabel();
--- tabelHTML.php
+++ tabelHTML.php
@@ -14,4 +14,20 @@ class tabelHTML extends tabel {
         $this->setTaustaVarv($taustaVarv);
     }
 
+    function prindiTabel(){
+        echo '<table border="1">';
+        echo '<tr bgcolor="'.$this->taustaVarv.'">';
+
+        foreach ($this->pealkirjad as $pealkiri){
+            echo '<th>'.$pealkiri.'</th>';
+        }   echo '</tr>';
+
+        foreach ($this->tabeliSisu as $tabeliRida){
+            echo '<tr bgcolor="'.$this->taustaVarv.'">';
+            foreach ($tabeliRida as $element){
+                echo '<td>'.$element.'</td>';
+            }   echo '</tr>';
+        }
+        echo '</table>';
+    }
 }
