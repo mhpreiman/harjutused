e38bb9dbc1d5342c432e6613cd8b77d0cef85e41
mhpreiman <mhpreiman@gmail.com>
2018-02-05 09:04:50 +0200

    Määra põhivaate (main) muutujad

--- index.php
+++ index.php
@@ -4,6 +4,13 @@ require_once 'conf.php';
 
 $mainTmpl = new template('main');
 
+$mainTmpl->set('site_lang','et');
+$mainTmpl->set('site_title','PV');
+$mainTmpl->set('user','Kasutaja');
+$mainTmpl->set('title','Pealkiri');
+$mainTmpl->set('lang_bar','Keeleriba');
+$mainTmpl->set('menu','Lehe menüü');
+$mainTmpl->set('content','Lehe sisu');
 
 echo '<pre>';
 print_r($mainTmpl);
