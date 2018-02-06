9812185430775398600e2bc99562ffce58a4fb38
mhpreiman <mhpreiman@gmail.com>
2018-02-06 11:31:30 +0200

    Funktsioon ilusamate päringute kirjutamiseks

--- lib/utils.php
+++ lib/utils.php
@@ -1,6 +1,11 @@
 <?php
 
+// Encode string (replace all non-alphanumeric with corresponding values, eg space with +)
 function fixUrl($str){
-    //Encode string (replace all non-alphanumeric with corresponding values, eg space with +)
     return urlencode($str);
+}
+
+// Add necessary puntuation to database query make it look cleaner
+function fixDB($str){
+    return '"'.addslashes($str).'"';
 }
