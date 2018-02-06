c7f68d3adfdfc71a7a7f30faef80c695b2af9d23
mhpreiman <mhpreiman@gmail.com>
2018-02-06 06:17:11 +0200

    Loo päringustringi lisamine

--- models/linkobject.php
+++ models/linkobject.php
@@ -11,4 +11,15 @@ class linkobject extends http
         parent::__construct();
         $this->baseLink = $this->protocol.HTTP_HOST.SCRIPT_NAME;        //complete path incl protocol
     }
+
+
+    // Create query parameters and add them to query string     eg first=value&second=value
+    function addQueryParams(&$queryString, $queryParam, $queryParamVal){      //& - $link points to the same data as the var that called it (all changes affect it)
+        //Add delimiter & if query string already contains a parameter
+        if($queryString != ''){
+            $queryString .= $this->delim;  // param=val  ->  param=val&
+        }
+        //Add a query parameter and value to query string      ->  param=val / param=val&param2=val
+        $queryString .= $queryParam.$this->eq.$queryParamVal;
+    }
 }
