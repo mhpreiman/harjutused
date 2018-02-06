e01731b483519893fb48996a22675c2a2ec8000d
mhpreiman <mhpreiman@gmail.com>
2018-02-06 04:27:18 +0200

    Loo http klass

---
+++ models/http.php
@@ -0,0 +1,24 @@
+<?php
+
+class http
+{
+    var $queryParameters = array();      //query parameters received through GET/POST    eg 2 vars name=Joe&age=24
+    var $server = array();               //server info
+
+    public function __construct(){
+        $this->init();
+    }
+
+    // Initialize
+    function init(){
+        $this->queryParameters = array_merge($_GET, $_POST);      //merge GET and POST arrays
+        $this->server = $_SERVER;                                 //get server info array
+
+        $httpConstants = array('HTTP_HOST','SCRIPT_NAME');
+        foreach ($httpConstants as $httpConstant){
+            if(!defined($httpConstant) and isset($this->server[$httpConstant])){
+                define($httpConstant, $this->server[$httpConstant]);
+            }
+        }
+    }
+}
